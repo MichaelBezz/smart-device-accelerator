9fde73ea476c9e31925038e4decd17e7c364c224
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-10-22 10:44:11 +0300

    Фиксит имена переменных

--- source/js/utils/webp-format.js
+++ source/js/utils/webp-format.js
@@ -1,4 +1,4 @@
-export const checkWebp = () => {
+export const checkWebpFormat = () => {
   const webP = new Image();
   webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
 
@@ -6,7 +6,7 @@ export const checkWebp = () => {
     if (webP.height === 2) {
       document.body.classList.add('js-webp');
     } else {
-      document.body.classList.add('js-no-webp');
+      document.body.classList.add('js-webp-exception');
     }
   };
 };
