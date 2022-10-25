fb01f49ba16eb5c6cf86b3dcd4fd620b0fe4f93c
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-10-25 22:14:45 +0300

    Фиксит валидацию формы

--- source/js/modules/form.js
+++ source/js/modules/form.js
@@ -1,15 +1,12 @@
+import iMask from 'imask';
+
 const forms = document.querySelectorAll('[data-form]');
 const headerButton = document.querySelector('[data-open-modal=feedback]');
 const modalFormName = document.querySelector('[data-md-form-name]');
 
-const ALLOWED_KEYS = [
-  'Backspace',
-  'Tab',
-  'ArrowUp',
-  'ArrowRight',
-  'ArrowDown',
-  'ArrowLeft'
-];
+const maskOptions = {
+  mask: '+{7}(000)0000000',
+};
 
 export const setModalFormFocus = () => {
   if (headerButton && modalFormName) {
@@ -20,46 +17,10 @@ export const setModalFormFocus = () => {
 };
 
 const initializeValidation = (form) => {
-  const formPhone = form.querySelector('[name="customer-phone"]');
+  const phoneInput = form.querySelector('[name="customer-phone"]');
 
-  if (formPhone) {
-    const phonePattern = /[0-9]/;
-
-    const onFormPhoneKeydown = (event) => {
-      if (!phonePattern.test(event.key) && !ALLOWED_KEYS.includes(event.key)) {
-        event.preventDefault();
-      }
-
-      if (formPhone.value.length === 7 && event.key === 'Backspace') {
-        formPhone.value = formPhone.value.substring(0, 6);
-      }
-
-      if (formPhone.value.length === 3 && event.key === 'Backspace') {
-        event.preventDefault();
-      }
-    };
-
-    formPhone.addEventListener('focus', () => {
-      if (!formPhone.value) {
-        formPhone.value = '+7(';
-      }
-
-      formPhone.addEventListener('keydown', onFormPhoneKeydown);
-    });
-
-    formPhone.addEventListener('input', () =>{
-      if (formPhone.value.length === 6) {
-        formPhone.value += ')';
-      }
-    });
-
-    formPhone.addEventListener('blur', () => {
-      if (formPhone.value === '+7(') {
-        formPhone.value = '';
-      }
-
-      formPhone.removeEventListener('keydown', onFormPhoneKeydown);
-    });
+  if (phoneInput) {
+    iMask(phoneInput, maskOptions);
   }
 };
 
